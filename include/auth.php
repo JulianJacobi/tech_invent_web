813440a27ee628391fe9c987ff63ea56130d6169
Mkalte <mkalte666@googlemail.com>
2013-03-28 21:49:34 +0100

    Sicherheit erweitert

--- include/auth.php
+++ include/auth.php
@@ -8,17 +8,21 @@
 
 $raw_login_user = "";
 $raw_login_passwd = "";
+if (!isset($_SESSION["login"])) {
+	$_SESSION["login"] = false;
+}
 if (isset($_POST["login_user"]) and isset($_POST["login_passwd"])) {
 	$raw_login_user = $_POST["login_user"];
 	$raw_login_passwd = $_POST["login_passwd"];
 }
-if($_SESSION["login"] != "true") {
+
+if($_SESSION["login"] == false) {
 generate_login(false);
 }
 else if($raw_login_user == "" or $raw_login_passwd == "" ) {
 generate_login(true);
-}
-else if($_SESSION["login"] == "true") {
+}       
+else if($_SESSION["login"] == true) {
 	//continue
 	}
 else {
