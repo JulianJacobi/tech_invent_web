2a6632726411ca8949a478343964d8d812318a10
Julian Jacobi <julian.jacobi@gmx.net>
2013-03-28 17:47:22 +0100

    html_footer

--- index.php
+++ index.php
@@ -6,10 +6,20 @@
  * @version 2013-03-28
  */
 
+//Login Scripts
 require_once("include/auth.php");
 
-include("templates/html_header");
+//HTML Header
+include("templates/html_head.php");
 
+//Navigationbar
 include("include/navigation.php");
+
+
+
+
+//HTML Footer
+include("templates/html_footer.php");
+
 
 ?>
--- templates/html_head.php
+++ templates/html_head.php
@@ -1,6 +1,7 @@
+<html>
 <head>
-	<meta name="content" content="Webseite der Technik AG des CJD Braunschweig" />
-	<meta name="author" content="Julian Jacobi" />
+	<meta name="content" content="TechInvent" />
+	<meta name="author" content="Technik AG CJD Braunschweig" />
 	<meta name="keywords" lang="de" content="CJD, Braunschweig, Technik" />
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 </head>
