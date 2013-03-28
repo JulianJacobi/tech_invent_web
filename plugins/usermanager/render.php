ac93925bc9b4516d17d37d188510b61d747b42d6
Kenakapheus <lukaskl@online.de>
2013-03-29 00:00:22 +0100

    Added Userlist

--- plugins/usermanager/render.php
+++ plugins/usermanager/render.php
@@ -1,7 +1,16 @@
 <?php
-echo "test";
+
 if (isset($_GET['mode']) && $_GET['mode'] == "show_users") {
-    echo "test";
+    global $config_db_server, $config_db_user, $config_db_password, $config_db_database;
+    $login_connection = mysql_connect($config_db_server, $config_db_user, $config_db_password);
+	mysql_select_db($config_db_database);
+	$e = mysql_query("SELECT * FROM login");
+    while($row = mysql_fetch_assoc($e)) {
+        $id = $row['id'];
+        $name = $row['username'];
+        $pass = $row['password'];
+        echo "Id:$id, Name:$name, Pass:$pass<br>";
+    }
 }
 
 ?>
