03d463068ca008ca06064245f6c243f4ae5e9e91
Mkalte <mkalte666@googlemail.com>
2013-03-28 21:06:51 +0100

    test_1

--- include/auth.php
+++ include/auth.php
@@ -2,10 +2,27 @@
 /*
  * Authentication for TechInvent
  *
- * @author Julian Jacobi
+ * @author Malte Kießling
  * @version 2013-03-28
  */
 
 require_once("config/config_db.php");
+$raw_login_user = $_POST["login_user"];
+$raw_login_passwd = $_POST["login_passwd"];
+if(isset($raw_login_user) and isset($raw_login_passwd)) {
+	if ($raw_login_user == "" and $raw_login_passwd == "") {
+		generate_login(false);
+	}
+}
+else {
+	generate_login(false);
+}
+
+
+function generate_login($error)
+{
+
+exit;
+}
 
 ?>
