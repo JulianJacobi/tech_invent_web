d073c9f5eb78025b838f4d2c0abc6c340539c4b2
Julian Jacobi <julian.jacobi@gmx.net>
2013-03-28 20:00:47 +0100

    little correction

--- include/navigation.php
+++ include/navigation.php
@@ -16,7 +16,7 @@ echo "<div id='menu'>";
 foreach ($nav_menu AS $plugin => $plugin_menu) {
   echo "<div class='menu_plugin'>";
   foreach ($plugin_menu AS $name => $get_val) {
-    echo "<div class='menu_item'><a href='/?plugin=$plugin&mode=$get_val'>$name</a></div>";
+    echo "<div class='menu_item'><a href='./?plugin=$plugin&mode=$get_val'>$name</a></div>";
   }
   echo "</div>";
 }
