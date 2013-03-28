7933973b96e65e10f1e5b11ec760ef883b2d157a
Julian Jacobi <julian.jacobi@gmx.net>
2013-03-28 20:43:37 +0100

    menu style

--- templates/html_head.php
+++ templates/html_head.php
@@ -4,4 +4,7 @@
 	<meta name="author" content="Technik AG CJD Braunschweig" />
 	<meta name="keywords" lang="de" content="CJD, Braunschweig, Technik" />
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	
+	<link type="text/css" rel="stylesheet" href="css/main.css">
+	<link type="text/css" rel="stylesheet" href="css/menu.css">
 </head>
