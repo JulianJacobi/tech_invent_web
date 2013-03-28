2e72ded8bd3f508ebab68176896a34ea2f91ca14
Julian Jacobi <julian.jacobi@gmx.net>
2013-03-28 20:51:47 +0100

    login form

---
+++ templates/login_form.php
@@ -0,0 +1,7 @@
+<div id="login_form">
+	<form action="./" method="post">
+		<input type="text" name="login_user">
+		<input type="password" name="login_passwd">
+		<input type="submit" value="login">
+	</form>
+</div>
