3f1ef8c683b10b3ad0fed4f8b7f8e991f983df7f
Mkalte <mkalte666@googlemail.com>
2013-03-28 23:26:57 +0100

    Kommentare hinzugefügt!

--- include/auth.php
+++ include/auth.php
@@ -5,35 +5,47 @@
  * @author Malte Kießling
  * @version 2013-03-28
  */
+ 
+ //Einfügen der Datenbank-headers
 include("config/config_db.php");
+
+//Erstellen und leeren der Roh-Variablen
  
 $raw_login_user = "";
 $raw_login_passwd = "";
+
+//Ist die Session erstellt?
 if (!isset($_SESSION["login"])) {
 	$_SESSION["login"] = false;
 	generate_login(false);
 	
 }
+
+//Wird was wer POST übergeben?
 if (isset($_POST["login_user"]) and isset($_POST["login_passwd"])) {
 	$raw_login_user = $_POST["login_user"];
-	$raw_login_passwd = $_POST["login_passwd"];
-	
+	$raw_login_passwd = $_POST["login_passwd"];	
 }
 
-
-
+//Bin ich angemeldet?
 if($_SESSION["login"] == true) {
 	//continue
 	}
+	
+//Denn wenn nicht...
 else {
+	//...schaue ich, ob man überhabt was übergeben hat,
 	if($raw_login_passwd == "" and $raw_login_user == "") {
 		generate_login(false);
 	}
 	
+	//dafür sorgen, das auch ja nimand hackt,...
 	$escaped_login_user = mysql_real_escape_string($raw_login_user);
 	if($escaped_login_user != $raw_login_user){
 		generate_login(true);
 		}
+		
+	//und hab dann hier ne challange für den login!
 	$hashed_login_passwd = md5($raw_login_passwd);
 	$login_connection = mysql_connect($config_db_server, $config_db_user, $config_db_password);
 	$abfrage = "SELECT * FROM login WHERE username == '$escaped_login_user' LIMIT 1";
@@ -41,15 +53,19 @@ else {
 	$row = mysql_fetch_object($ergebnis);
     
 	if(md5($row->username.$row->password) == md5($escaped_login_user.$hashed_login_passwd)) {
+		//Eintragen der Darten in die Session
 		$_SESSION["login"] = true;
 		$_SESSION["userid"] = $row->id;
 		$_SESSION["username"] = $row->username;
 		}
 	else
 	{
+		//... oder auch nicht
 		generate_login(true);
 		}
 }
+
+//Das is die Funktion, die im falle das Irgentwas mit Login passiert aufgerufen wird.
 function generate_login($error)
 {
 include("templates/login_form.php");
