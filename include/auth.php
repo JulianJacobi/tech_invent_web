a56dbbf656bd4fe64b3b0a3c74bde4d195f53a0d
Mkalte <mkalte666@googlemail.com>
2013-03-28 23:38:47 +0100

    lkj

--- include/auth.php
+++ include/auth.php
@@ -50,7 +50,7 @@ else {
 	$login_connection = mysql_connect($config_db_server, $config_db_user, $config_db_password);
 	mysql_select_db($config_db_database);
 	$abfrage = "SELECT * FROM login WHERE username == '$escaped_login_user' LIMIT 1";
-	$ergebnis = mysql_query($abfrage);
+	$ergebnis = mysql_query($abfrage); 
 	$row = mysql_fetch_object($ergebnis);
     if(isset($row->username) and isset($row->password) and isset($row->id)) { 
 		if(md5($row->username.$row->password) == md5($escaped_login_user.$hashed_login_passwd)) {
