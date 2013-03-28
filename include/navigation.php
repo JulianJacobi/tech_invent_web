f08ccf639d2e1b303d0d6d3152f54cc0642c3ac2
Kenakapheus <lukaskl@online.de>
2013-03-28 19:25:37 +0100

    Added Menurederer

--- include/navigation.php
+++ include/navigation.php
@@ -2,10 +2,25 @@
 /*
  * Menu Construktor for TechInvent
  *
- * @author Julian Jacobi
+ * @author Lukas Klingberg
  * @version 2013-03-28
  */
  
+$nav_menu['main']['Start'] = "main";
+$nav_menu['main']['Ger&auml;te'] = "things";
+$nav_menu['main']['Produktionen'] = "productions"; // main=core
+$nav_menu['show']['Produktionen'] = "productions";
 
+echo "<div id='menu'>";
+
+foreach ($nav_menu AS $plugin => $plugin_menu) {
+  echo "<div class='menu_plugin'>";
+  foreach ($plugin_menu AS $name => $get_val) {
+    echo "<div class='menu_item'><a href='/?plugin=$plugin&mode=$get_val'>$name</a></div>";
+  }
+  echo "</div>";
+}
+
+echo "</div>";
 
 ?>
