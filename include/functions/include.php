7e584374a98ba67fc9d82abda0452338dcd76717
Kenakapheus <lukaskl@online.de>
2013-03-28 20:36:00 +0100

    Fixed some major bugs in the menufunctions

--- include/functions/include.php
+++ include/functions/include.php
@@ -1,6 +1,6 @@
 <?php
 
-include ("include/methods/db_access.php")
-include ("include/methods/menu.php")
+include ("include/functions/db_access.php");
+include ("include/functions/menu.php");
 
 ?>
--- include/functions/menu.php
+++ include/functions/menu.php
@@ -1,6 +1,7 @@
 <?php
 
 function add_menu_item($plugin_identifier, $item_name, $item_get_value) {
+    global $nav_menu;
     $nav_menu[$plugin_identifier][$item_name] = $item_get_value;
 }
 
