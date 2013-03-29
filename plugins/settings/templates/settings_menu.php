59803f6626aada7058458973f36c6d5b87606443
Mkalte <mkalte666@googlemail.com>
2013-03-29 17:24:00 +0100

    settings update

--- plugins/settings/templates/settings_menu.php
+++ plugins/settings/templates/settings_menu.php
@@ -6,6 +6,6 @@
 				if ($modul_plugin_name == $plugin_name) {
 					foreach ($modul_plugin_arry AS $modul_name => $modul_printname) {
 	?>
-					<div class='settings_menu_item'><a href='./?plugin=settings&mode=<?php print($plugin_name); ?>&modul=<?php print($modul_name); ?>'><?php print($item_name." - ".$modul_name ); ?></a></div>
+					<div class='settings_menu_item'><a href='./?plugin=settings&mode=<?php print($plugin_name); ?>&modul=<?php print($modul_name); ?>'><?php print($modul_name); ?></a></div>
 	<?php }}}} ?>
 </div>
