236085af54a19516abe225e66dd52fda965b2b97
Kenakapheus <lukaskl@online.de>
2013-03-29 22:43:28 +0100

    backend

--- plugins/usermanager/settings_groups.php
+++ plugins/usermanager/settings_groups.php
@@ -4,6 +4,21 @@ include("plugins/usermanager/strings.php");
 if(!isset($_POST['step'])) {
 	global $strings;
 	include("plugins/usermanager/templates/grouplist.php");
+} elseif ($_POST['step'] == "edit_groups") {
+	$groupname = $_POST['gname'];
+	if (isset($_POST['edit']) && $_POST['edit'] == "perms") {
+		$groups = get_permissions_for_group($groupname);
+		foreach ($groups AS $name => $val) {
+			if (isset($_POST[$name]) && $_POST[$name] == "on") {
+				$groups[$name] = true;
+			} else {
+				$groups[$name] = false;
+			}
+		}
+		 set_permissions_for_group($groupname, $groups);
+	}
+	global $strings;
+	include("plugins/usermanager/templates/group_mono.php");
 }
 
 ?>
