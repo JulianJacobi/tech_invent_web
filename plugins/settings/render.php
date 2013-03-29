3c088c6a30e748175691dccfa8139c9b250963fa
Mkalte <mkalte666@googlemail.com>
2013-03-29 16:18:45 +0100

    bugfix settings

--- plugins/settings/render.php
+++ plugins/settings/render.php
@@ -1,6 +1,9 @@
 <?php
 global $settings_menu;
 global $settings_modules;
+if(!isset($_GET["modul"])) {
+$_GET["modul"] = "Haupteinstellungen";
+}
 include("plugins/settings/templates/settings_menu.php");
 include("plugins/settings/show.php");
 
--- plugins/settings/templates/settings_menu.php
+++ plugins/settings/templates/settings_menu.php
@@ -1,6 +1,7 @@
 <link href="plugins/settings/css/settings_menu.css" rel="stylesheet" type="text/css">
 <div id="settings_menu">
-	<?php foreach ($settings_menu AS $plugin_name => $item_name) { 
+	<?php
+	foreach ($settings_menu AS $plugin_name => $item_name) { 
 			foreach ($settings_modules AS $modul_plugin_name => $modul_plugin_arry) {
 				if ($modul_plugin_name == $plugin_name) {
 					foreach ($modul_plugin_arry AS $modul_name => $modul_printname) {
