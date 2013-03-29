4a02cf9d8a938284877b38c1abf922b8320dfdac
Julian Jacobi <julian.jacobi@gmx.net>
2013-03-29 11:28:08 +0100

    user_settings template bugfix

--- plugins/usermanager/templates/user_settings.php
+++ plugins/usermanager/templates/user_settings.php
@@ -1,13 +1,13 @@
 <?php global $strings; ?>
 <link href="plugins/usermanager/css/user_settings.css" rel="stylesheet" type="text/css">
 <div id="user_settings">
-	<form action="./" method="post">
-		<input type="text" name="settings_new_username">
+	<form action="./?plugin=usermanager&mode=user_settings" method="post">
+		<input type="text" name="settings_new_username" class="system_input_text">
 		
-		<input type="password" name="settings_old_passwd">
-		<input type="password" name="settings_new_passwd">
-		<input type="password" name="settings_new_passwd2">
+		<input type="password" name="settings_old_passwd" class="system_input_text">
+		<input type="password" name="settings_new_passwd" class="system_input_text">
+		<input type="password" name="settings_new_passwd2" class="system_input_text">
 		
-		<input type="submit" value="Ändern">
+		<input type="submit" value="Ändern" class="system_button">
 	</form>
 </div>
