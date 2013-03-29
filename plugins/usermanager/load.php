a0ff49ee30aab8e2876a88a20a321d70ce1b098e
Julian Jacobi <julian.jacobi@gmx.net>
2013-03-29 18:29:04 +0100

    bug

--- plugins/usermanager/load.php
+++ plugins/usermanager/load.php
@@ -2,6 +2,6 @@
 
 //add_menu_item('usermanager', 'Benutzerliste', "show_users"); // main=core
 //add_menu_item('usermanager', 'Nutzereinstellungen', "user_settings"); // main=core
-add_settings_item("usermanager", "Nutzereinstellungen", "Daten", "settings");
-add_settings_item("usermanager", "Nutzereinstellungen", "Nutzerliste", "settings_userlist");
+add_settings_item("usermanager", "Nutzereinstellungen", "Nutzerdaten", "settings");
+add_settings_item("usermanager", "Nutzereinstellungen", "Benutzerverwaltung", "settings_userlist");
 ?>
--- plugins/usermanager/templates/userlist.php
+++ plugins/usermanager/templates/userlist.php
@@ -9,6 +9,14 @@
 		</td><td>
 			<p class="usermanager_userlist_head"><?php print($strings['usermanager']['userlist_head_pass']); ?></p>
 		</td></tr>
+		<tr><td></td><td></td><td></td><td>
+			<form action="./?plugin=settings&mode=usermanager&modul=Nutzerliste" method="post">
+				<input type="hidden" name="step" value="user_mono">
+				<input type="hidden" name="uid" value="<?php print($i['id']) ?>">
+				<input type="hidden" name="uname" value="<?php print($i['name']) ?>">
+				<input type="submit" value="<?php print($strings['usermanager']['userlist_edit']) ?>" class="system_button usermanager_userlist_editbutton">
+			</form>
+		</td></tr>
 	<?php $j = 0; ?>
 	<?php foreach ($users AS $i){ ?>
 		<tr class="usermanager_userlist_entry <?php if($j % 2 == 1){ print("usermanager_userlist_entry_hl");} ?>"><td>
