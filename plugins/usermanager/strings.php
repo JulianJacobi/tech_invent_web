128d1db041385183f98bc33c0eca513449047aef
Julian Jacobi <julian.jacobi@gmx.net>
2013-03-29 21:04:41 +0100

    frontends for deluser and userlist

--- plugins/usermanager/strings.php
+++ plugins/usermanager/strings.php
@@ -39,7 +39,14 @@ $strings['usermanager']['newuser_repeat_passwd']			= "Wiederholung:";
 $strings['usermanager']['newuser_add']						= "Hinzufügen";
 $strings['usermanager']['newuser_error1']					= "Benutzername ungültig!";
 $strings['usermanager']['newuser_error2']					= "Passwort ungültig!";
-$strings['usermanager']['newuser_error3']					= "Passwörter stimmen nicht überein!"
+$strings['usermanager']['newuser_error3']					= "Passwörter stimmen nicht überein!";
+
+//deluser
+$strings['usermanager']['deluser_headline']					= "Benutzer löschen";
+$strings['usermanager']['deluser_question1']				= "Bist du sicher, dass du den Benutzer ";
+$strings['usermanager']['deluser_question2']				= " unwiederuflich löschen möchtest?";
+$strings['usermanager']['deluser_ok']						= "OK";
+$strings['usermanager']['deluser_abbr']						= "Abbrechen";
 
 
 
---
+++ plugins/usermanager/templates/del_user.php
@@ -0,0 +1,12 @@
+<link href="plugins/usermanager/css/del_user.css" rel="stylesheet" type="text/css">
+<div id="usermanager_deluser">
+	<p id="usermanager_deluser_headline"><?php print($strings['usermanager']['deluser_headline']); ?></p>
+	<p id="usermanager_deluser_label"><?php print($strings['usermanager']['deluser_question1']."<b>".$_POST['uname']."</b>".$strings['usermanager']['deluser_question2']); ?></p>
+	<form action="./?plugin=settings&mode=usermanager&modul=Benutzerverwaltung" method="post" id="usermanager_deluser">
+		<input type="hidden" value="<?php print($_POST['uname']) ?>" name="uname">
+		<input type="hidden" value="del_user" name="step">
+		<input type="hidden" value="true" name="delete">
+		<a href="./?plugin=settings&mode=usermanager&modul=Benutzerverwaltung" class="usermanager_deluser_link"><button class="system_button" type="button"><?php print($strings['usermanager']['deluser_abbr']) ?></button></a>
+		<input type="submit" value="<?php print($strings['usermanager']['deluser_ok']) ?>" class="system_button">
+	</form>
+</div>
