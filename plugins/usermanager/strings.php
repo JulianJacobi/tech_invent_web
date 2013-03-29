a7087f9389bcc8d08bd94c9d37263c78efb91c21
Julian Jacobi <julian.jacobi@gmx.net>
2013-03-29 13:16:10 +0100

    user_settings template final!

--- plugins/usermanager/strings.php
+++ plugins/usermanager/strings.php
@@ -12,9 +12,12 @@ global $strings;
 
 $strings['usermanager']['user_settings_headline']			= "Benutzereinstellungen ändern";
 $strings['usermanager']['user_settings_username']			= "Benutzername:";
-$strings['usermanager']['user_settings_old_passwd']			= "Altes Passwort:";
+$strings['usermanager']['user_settings_old_passwd']			= "Passwort:";
 $strings['usermanager']['user_settings_new_passwd']			= "Neues Passwort:";
 $strings['usermanager']['user_settings_repeat_passwd']		= "Wiederholung:";
 
+$strings['usermanager']['user_settings_error1']				= "Passwörter stimmen nicht überein!";
+$strings['usermanager']['user_settings_error2']				= "Passwort falsch!";
+$strings['usermanager']['user_settings_error3']				= "erfolgreich!";
 $strings['usermanager']['user_settings_error4']				= "Beutzername existiert schon!";
 ?>
--- plugins/usermanager/templates/user_settings.php
+++ plugins/usermanager/templates/user_settings.php
@@ -1,6 +1,11 @@
 <link href="plugins/usermanager/css/user_settings.css" rel="stylesheet" type="text/css">
 <div id="user_settings">
-	<p id="usermanager_user_settings_headline"><?php print($strings['usermanager']['user_settings_headline']); ?>
+	<p id="usermanager_user_settings_headline"><?php print($strings['usermanager']['user_settings_headline']); ?></p>
+	<?php
+	if($setting_state == 3) {
+		print('<p class="usermanager_user_settings_success">'.$strings['usermanager']['user_settings_error3'].'</p>');
+	}
+	?>
 	<form action="./?plugin=usermanager&mode=user_settings" method="post" id="usermanager_user_settings">
 		<p class="usermanager_user_settings_label"><?php print($strings['usermanager']['user_settings_username']); ?></p>
 		<?php
@@ -11,10 +16,20 @@
 		<input type="text" name="settings_new_username" class="system_input_text usermanager_user_settings" value="<?php print($settings_new_username); ?>">
 		
 		<p class="usermanager_user_settings_label"><?php print($strings['usermanager']['user_settings_old_passwd']); ?></p>
+		<?php
+		if($setting_state == 2) {
+			print('<p class="usermanager_user_settings_error">'.$strings['usermanager']['user_settings_error2'].'</p>');
+		}
+		?>
 		<input type="password" name="settings_old_passwd" class="system_input_text usermanager_user_settings">
 		<p class="usermanager_user_settings_label"><?php print($strings['usermanager']['user_settings_new_passwd']); ?></p>
 		<input type="password" name="settings_new_passwd" class="system_input_text usermanager_user_settings">
 		<p class="usermanager_user_settings_label"><?php print($strings['usermanager']['user_settings_repeat_passwd']); ?></p>
+		<?php
+		if($setting_state == 1) {
+			print('<p class="usermanager_user_settings_error">'.$strings['usermanager']['user_settings_error1'].'</p>');
+		}
+		?>
 		<input type="password" name="settings_new_passwd2" class="system_input_text usermanager_user_settings"><br>
 		
 		<input type="submit" value="Ändern" class="system_button usermanager_user_settings_button">
