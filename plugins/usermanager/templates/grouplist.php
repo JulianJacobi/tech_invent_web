429acee0967f9659af045f04bd61fa4661bf7374
Kenakapheus <lukaskl@online.de>
2013-03-30 12:48:23 +0100

    valign

--- plugins/usermanager/templates/grouplist.php
+++ plugins/usermanager/templates/grouplist.php
@@ -39,14 +39,14 @@
 			<p><?php print("$row->name"); ?></p>
 		</td><td>
 			<p class="usermanager_grouplist_description"><?php print($description); ?></p>
-		</td><td valign="bottom">
+		</td><td valign="middle">
 			<form action="./?plugin=settings&mode=usermanager&modul=Rechtegruppen" method="post">
 				<input type="hidden" name="step" value="edit_groups">
 				<input type="hidden" name="edit" value="perms">
 				<input type="hidden" name="gname" value="<?php print("$row->name"); ?>">
 				<input type="submit" value="<?php print($strings['usermanager']['grouplist_edit']); ?>" class="system_button usermanager_grouplist_button">
 			</form>
-		</td><td valign="bottom">
+		</td><td valign="middle">
 			<form action="./?plugin=settings&mode=usermanager&modul=Rechtegruppen" method="post">
 				<input type="hidden" name="step" value="del_groups">
 				<input type="hidden" name="gname" value="<?php print("$row->name"); ?>">
