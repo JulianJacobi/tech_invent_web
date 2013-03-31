ed1056c3b8a3710771dba93e7394e942d48650d9
Kenakapheus <lukaskl@online.de>
2013-03-31 15:02:51 +0200

    Mergefix

--- plugins/events/load.php
+++ plugins/events/load.php
@@ -2,8 +2,8 @@
 
 include ('plugins/events/strings.php');
 
-add_menu_item("events", $strings['events']['main_menu_entry']);
+$menu->addMenuItem("events", $strings['events']['main_menu_entry']);
 
-add_submenu_item("events", $strings['events']['sub_menu_calendar'], "calendar");
+$menu->addSubmenuItem("events", $strings['events']['sub_menu_calendar'], "calendar");
 
 ?>
