61768f65810f63e03b3dd6d65545d0655a2ea534
Kenakapheus <lukaskl@online.de>
2013-03-31 16:54:34 +0200

    Inventory object

--- plugins/inventorysystem/Inventory.php
+++ plugins/inventorysystem/Inventory.php
@@ -2,11 +2,22 @@
 
 class Inventory
 {
-	protected $name = "";
+	protected $id = 0;
+	public $name = "";
+	public $description = "";
 	
 	protected $items = array();
-	protected $count = 0;
+	public $item_count = 0;
 	
+	function __construct($name) {
+		$this->name = $name;
+		$result = mysql_query("SELECT * FROM inventorys WHERE name = '" . $name . "';");
+		if (mysql_num_rows($result) > 0) {
+			$row = mysql_fetch_object($result);
+			$this->description = $row->description;
+			$this->id = $row->id;
+		}
+	}
 }
 
 ?>
