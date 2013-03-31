76a9879f613eccb71ee52e1e460c54b00585a2c9
Julian Jacobi <julian.jacobi@gmx.net>
2013-03-31 17:34:04 +0200

    sync

--- plugins/inventorysystem/strings.php
+++ plugins/inventorysystem/strings.php
@@ -1,8 +1,11 @@
 <?php
 
-$strings['menu']['pluginname'] 						= "Inventar";
-$strings['menu']['inventorylist'] 					= "Inventarliste";
+$strings['menu']['pluginname'] 									= "Inventar";
+$strings['menu']['inventorylist'] 								= "Inventarliste";
 
-$strings['inventorysystem']['inventorylist']		= "Inventarliste";
+$strings['inventorysystem']['inventorylist']					= "Inventarliste";
+$strings['inventorysystem']['inventorylist_name']				= "Name";
+$strings['inventorysystem']['inventorylist_count']				= "Elemente";
+$strings['inventorysystem']['inventorylist_description']		= "Beschreibung";
 
 ?>
--- plugins/inventorysystem/templates/InventoryList.php
+++ plugins/inventorysystem/templates/InventoryList.php
@@ -1,5 +1,34 @@
 <link href="plugins/inventorysystem/css/InventoryList.css" rel="stylesheet" type="text/css">
 <div class="templates_container inventorysystem_inventorylist">
 	<p class="templates_headline"><?php print($strings['inventorysystem']['inventorylist']); ?></p>
-	<?php print $inv['ding']->name ?>
+	<table class="inventorysystem_inventorylist" align="center">
+	<tr class="inventorysystem_inventorylist_head"><td>
+		<p><?php print($strings['inventorysystem']['inventorylist_name']); ?></p>
+	</td><td>
+		<p><?php print($strings['inventorysystem']['inventorylist_count']); ?></p>
+	</td><td>
+		<p><?php print($strings['inventorysystem']['inventorylist_description']); ?></p>
+	</td></tr>
+	
+	<tr class="inventorysystem_inventorylist_add"><td>
+	
+	</td><td>
+	
+	</td><td>
+	
+	</td><td>
+	
+	</td><td></td></tr>
+	<?php
+		foreach ($inv AS $name => $inventory) {
+			print('<tr class="inventorysystem_inventorylist_entry"><td class="inventorysystem_inventorylist_entry_name">');
+			print("$inventory->name");
+			print('</td><td class="inventorysystem_inventorylist_entry_count">');
+			print("$inventory->item_count");
+			print('</td><td class="inventorysystem_inventorylist_entry_description">');
+			print("$inventory->description");
+			print('</td></tr>');
+		}
+	?>
+	</table>
 </div>
